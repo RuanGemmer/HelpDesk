c6edd0e7df92d4d3be5843b60f7e91243b0b7932
RuanGemmer <ruangemmer@live.com>
2023-06-14 20:29:07 -0300

    Projeto Iniciado

---
+++ src/main/java/com/ruan/helpdesk/HelpsedkApplication.java
@@ -0,0 +1,14 @@
+package com.ruan.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpsedkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpsedkApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/ruan/helpdesk/HelpsedkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruan.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpsedkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
