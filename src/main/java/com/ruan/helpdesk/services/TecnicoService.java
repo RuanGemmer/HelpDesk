809bbc9dd45be2976df3d299421f6beb5bac781b
RuanGemmer <ruangemmer@live.com>
2023-06-26 15:57:02 -0300

    End point criado para Tecnico

--- src/main/java/com/ruan/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ruan/helpdesk/services/TecnicoService.java
@@ -1,11 +1,15 @@
 package com.ruan.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Service;
 
 import com.ruan.helpdesk.domain.Tecnico;
+import com.ruan.helpdesk.domain.dtos.TecnicoDTO;
 import com.ruan.helpdesk.repository.TecnicoRepository;
 import com.ruan.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -18,5 +22,15 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
