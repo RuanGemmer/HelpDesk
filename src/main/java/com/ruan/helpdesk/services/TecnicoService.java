8d83bfdb08203d129bcf43fc74cf67b65c81768a
RuanGemmer <ruangemmer@live.com>
2023-06-26 13:49:46 -0300

    Adicionado exceção para objeto não encontrado

--- src/main/java/com/ruan/helpdesk/services/TecnicoService.java
+++ src/main/java/com/ruan/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ruan.helpdesk.domain.Tecnico;
 import com.ruan.helpdesk.repository.TecnicoRepository;
+import com.ruan.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
 }
