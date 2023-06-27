ec482c64fe494e0a4aade5c0b0d1679b7be80440
RuanGemmer <ruangemmer@live.com>
2023-06-27 11:02:28 -0300

    Criado método de busca por ID para Chamados

---
+++ src/main/java/com/ruan/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.ruan.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ruan.helpdesk.domain.Chamado;
+import com.ruan.helpdesk.repository.ChamadoRepository;
+import com.ruan.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById (Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! ID: " + id));
+	}
+}
