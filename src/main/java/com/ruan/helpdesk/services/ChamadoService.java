951152e19af33d36bb6d5a98d5acee2e882dee53
RuanGemmer <ruangemmer@live.com>
2023-06-27 11:09:41 -0300

    Criado método FindAll para Chamados

--- src/main/java/com/ruan/helpdesk/services/ChamadoService.java
+++ src/main/java/com/ruan/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.ruan.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class ChamadoService {
 	public Chamado findById (Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado! ID: " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 }
